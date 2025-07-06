0dd61ad93d8edbcda6d1031fa1c27a989afef6fc
cmlorbert <cmlorbert@gmail.com>
2025-07-06 14:32:24 -0500

    Completed assignment

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,32 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
-
-let result = "5" - 2;
+ 
+let result = "5" - 2; // This is valid and will execute. JS implicitly converts the string to a number for subtraction
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false"); // This executes bc a string will translate as a Truthy value. To make false, make a Falsey value. I left as is bc it is a truthy value.
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5; // Need change age to a number otherwise this gives you totalAge = 255. Change made.
 console.log("Total Age: " + totalAge);
+
+// implicit type
+let jerseyNumber = "88";
+let jerseyNumberTimesOne= jerseyNumber * 1;
+console.log(jerseyNumberTimesOne);
+
+// explicit type
+jerseyNumber = "89";
+let jerseyNumberPlusOne= Number(jerseyNumber) + 1;
+console.log(jerseyNumberPlusOne);
+
+// this print statement wont execute bc null is false
+let bike = null
+if (bike) {
+  console.log("This is truthy");
+}
+
